e848d7d498e22ea8b5ecc9b8429a8bafc3b6a0f0
helllth <th@cismet.de>
2014-07-24 21:21:18 +0200

    working on #30: switch from a hardcoded runtimeContainer to a fairly simple container (maybe drop we'll it in the future)

--- src/main/java/de/cismet/cids/server/rest/domain/RuntimeContainer.java
+++ src/main/java/de/cismet/cids/server/rest/domain/RuntimeContainer.java
@@ -9,21 +9,6 @@ package de.cismet.cids.server.rest.domain;
 
 import com.sun.jersey.api.client.Client;
 
-import de.cismet.cids.server.rest.cores.ActionCore;
-import de.cismet.cids.server.rest.cores.EntityCore;
-import de.cismet.cids.server.rest.cores.EntityInfoCore;
-import de.cismet.cids.server.rest.cores.NodeCore;
-import de.cismet.cids.server.rest.cores.PermissionCore;
-import de.cismet.cids.server.rest.cores.SearchCore;
-import de.cismet.cids.server.rest.cores.UserCore;
-import de.cismet.cids.server.rest.cores.dummy.DummyPermissionCore;
-import de.cismet.cids.server.rest.cores.dummy.DummyUserCore;
-import de.cismet.cids.server.rest.cores.filesystem.FileSystemActionCore;
-import de.cismet.cids.server.rest.cores.filesystem.FileSystemEntityCore;
-import de.cismet.cids.server.rest.cores.filesystem.FileSystemEntityInfoCore;
-import de.cismet.cids.server.rest.cores.filesystem.FileSystemNodeCore;
-import de.cismet.cids.server.rest.data.unused.CustomAttributeCore;
-
 /**
  * DOCUMENT ME!
  *
@@ -34,65 +19,7 @@ public class RuntimeContainer {
 
     //~ Static fields/initializers ---------------------------------------------
 
-    static final String DIR = Starter.FS_CIDS_DIR;
-    static FileSystemEntityCore fsec = new FileSystemEntityCore(DIR);
-    static FileSystemActionCore fsac = new FileSystemActionCore(DIR);
-    static FileSystemEntityInfoCore fseic = new FileSystemEntityInfoCore(DIR);
-    static FileSystemNodeCore fsnc = new FileSystemNodeCore(DIR);
-    static DummyUserCore duc = new DummyUserCore();
-    static DummyPermissionCore dpc = new DummyPermissionCore();
-    static Server server = new Server() {
-
-            @Override
-            public EntityInfoCore getEntityInfoCore() {
-                return fseic;
-            }
-
-            @Override
-            public PermissionCore getPermissionCore() {
-                return dpc;
-            }
-
-            @Override
-            public EntityCore getEntityCore(final String classKey) {
-                return fsec;
-            }
-
-            @Override
-            public CustomAttributeCore getCustomAttributeCore() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            @Override
-            public NodeCore getNodeCore() {
-                return fsnc;
-            }
-
-            @Override
-            public ActionCore getActionCore() {
-                return fsac;
-            }
-
-            @Override
-            public SearchCore getSearchCore() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            @Override
-            public UserCore getUserCore() {
-                return duc;
-            }
-
-            @Override
-            public String getDomainName() {
-                return "CRISMA";
-            }
-
-            @Override
-            public String getRegistry() {
-                return ServerConstants.STANDALONE;
-            }
-        };
+    static Server server;
 
     //~ Methods ----------------------------------------------------------------
 
@@ -103,6 +30,15 @@ public class RuntimeContainer {
      */
     public static Server getServer() {
         return server;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  s  DOCUMENT ME!
+     */
+    public static void setServer(final Server s) {
+        server = s;
     }
 
     /**
