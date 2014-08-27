fc95dc814704236a08a441ff486a30bc52c82125
Daniel Meiers <daniel.meiers@cismet.de>
2014-08-27 16:16:32 +0200

    #48 integrate custom version of swagger ui
    integrated a custom version of swagger ui with 2 form fields for
    entering credentials.
    Furthermore this custom swagger ui version is hosted on the jetty web
    server started for the rest api

--- src/main/java/de/cismet/cids/server/api/tools/CORSResponseFilter.java
+++ src/main/java/de/cismet/cids/server/api/tools/CORSResponseFilter.java
@@ -26,6 +26,7 @@ public final class CORSResponseFilter implements ContainerResponseFilter {
         cr1.getHttpHeaders().add("Access-Control-Allow-Origin", "*");                                // NOI18N
         cr1.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS"); // NOI18N
         cr1.getHttpHeaders().add("Access-Control-Allow-Headers", "Content-Type");                    // NOI18N
+        cr1.getHttpHeaders().add("Access-Control-Allow-Headers", "Authorization");                   // NOI18N
 
         return cr1;
     }
