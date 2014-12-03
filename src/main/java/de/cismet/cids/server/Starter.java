ebc27711496a0633731abf9420daddce9ee1dec0
Martin Scholl <martin.scholl@cismet.de>
2014-12-03 18:31:20 +0100

    [issue #56] ssl connector and properties
    The server connector is now initialised according to the provided
    properties. Thus it now supports ssl connections using a certain
    certificate as well as client authentication via certificate. The ssl
    connector is disabled by default in order to be backwards compatible.

--- src/main/java/de/cismet/cids/server/Starter.java
+++ src/main/java/de/cismet/cids/server/Starter.java
@@ -16,25 +16,26 @@ import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 import com.wordnik.swagger.jaxrs.JaxrsApiReader;
 
-import org.apache.commons.io.IOUtils;
+import lombok.extern.slf4j.Slf4j;
 
+import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
 import org.mortbay.jetty.handler.ContextHandlerCollection;
 import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.security.SslSocketConnector;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
 
 import org.openide.util.Lookup;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Properties;
 
 import de.cismet.cids.server.api.ServerConstants;
 import de.cismet.cids.server.cores.CidsServerCore;
@@ -45,10 +46,16 @@ import de.cismet.cids.server.data.SimpleServer;
  * DOCUMENT ME!
  *
  * @author   thorsten
- * @version  $Revision$, $Date$
+ * @author   martin.scholl@cismet.de
+ * @version  0.1
  */
 @Parameters(separators = "=")
+@Slf4j
 public class Starter {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final int HEADER_BUFFER_SIZE = 512 * 1024; // = 512kb
 
     //~ Instance fields --------------------------------------------------------
 
@@ -86,12 +93,88 @@ public class Starter {
     boolean standalone = false;
 
     @Parameter(
+        names = { "-sslDebug" },
+        description = "If set to true the server will not use ssl connections, should not be true in production. "
+                    + "If set to false you have to provide a keystore that contains the server certificate or the server "
+                    + "will not start up properly.",
+        arity = 1
+    )
+    @SuppressWarnings("FieldMayBeFinal")
+    private boolean sslDebug;
+
+    @Parameter(
+        names = { "-sslKeystoreServer" },
+        description = "Path to the keystore holding the server's ssl certificate. The keystore should contain only one "
+                    + "certificate."
+    )
+    @SuppressWarnings("FieldMayBeFinal")
+    private String sslKeystoreServer;
+
+    @Parameter(
+        names = { "-sslKeystoreServerPassword" },
+        description = "The password of the keystore holding the server's ssl certificate. If it is not provided the "
+                    + "http container will ask for it which makes the startup interactive."
+    )
+    @SuppressWarnings("FieldMayBeFinal")
+    private String sslKeystoreServerPassword;
+
+    @Parameter(
+        names = { "-sslKeystoreServerKeyPassword" },
+        description = "The password of the server's ssl certificate private key entry. If it is not provided the http "
+                    + "container will ask for it which makes the startup interactive."
+    )
+    @SuppressWarnings("FieldMayBeFinal")
+    private String sslKeystoreServerKeyPassword;
+
+    @Parameter(
+        names = { "-sslClientAuth" },
+        description = "If set to true the server will accept trusted clients only, specified by the client keystore. "
+                    + "Then you have to provide keystore that contains the accepted client certificate or the server will "
+                    + "not start up properly.",
+        arity = 1
+    )
+    @SuppressWarnings("FieldMayBeFinal")
+    private boolean sslClientAuth;
+
+    @Parameter(
+        names = { "-sslKeystoreClient" },
+        description = "Path to the keystore holding the certificates of the trusted clients. Only clients with the key "
+                    + "corresponding to one of the accepted certificates are allowed to connect."
+    )
+    @SuppressWarnings("FieldMayBeFinal")
+    private String sslKeystoreClient;
+
+    @Parameter(
+        names = { "-sslKeystoreClientPassword" },
+        description = "The password of the keystore holding the certificates of the trusted clients. If it is not "
+                    + "provided the http container will ask for it which makes the startup interactive."
+    )
+    @SuppressWarnings("FieldMayBeFinal")
+    private String sslKeystoreClientPassword;
+
+    @Parameter(
         names = { "-enableCore", "-addmodule", "-modules", "-m", "--modules" },
         description = "active modules",
         variableArity = true,
         required = true
     )
     private List<String> activeModulesParameter = new ArrayList<String>();
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new Starter object.
+     */
+    public Starter() {
+        // default is true to ensure backwards compatibility
+        sslDebug = true;
+        sslKeystoreServer = null;
+        sslKeystoreServerPassword = null;
+        sslKeystoreServerKeyPassword = null;
+        sslClientAuth = false;
+        sslKeystoreClient = null;
+        sslKeystoreClientPassword = null;
+    }
 
     //~ Methods ----------------------------------------------------------------
 
@@ -100,6 +183,7 @@ public class Starter {
      *
      * @param  args  DOCUMENT ME!
      */
+    @SuppressWarnings("CallToPrintStackTrace")
     void init(final String[] args) {
         JCommander jcom = null;
         try {
@@ -136,65 +220,111 @@ public class Starter {
 
             final String swaggerBasePath = basePath + ":" + port;
             RuntimeContainer.setServer(cidsCoreHolder);
-            try {
-                JaxrsApiReader.setFormatString("");
-                final ServletHolder sh = new ServletHolder(ServletContainer.class);
-                sh.setInitParameter(
-                    "com.sun.jersey.config.property.packages",
-                    "de.cismet.cids.server.api;"
-                            // + "de.cismet.cids.server.rest.resourcelistings;"
-                            + "com.fasterxml.jackson.jaxrs");
+            JaxrsApiReader.setFormatString("");
+            final ServletHolder sh = new ServletHolder(ServletContainer.class);
+            sh.setInitParameter(
+                "com.sun.jersey.config.property.packages",
+                "de.cismet.cids.server.api;"
+                        // + "de.cismet.cids.server.rest.resourcelistings;"
+                        + "com.fasterxml.jackson.jaxrs");
 
-                sh.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature",
-                    "true");
-                sh.setInitParameter(
-                    "com.sun.jersey.spi.container.ContainerResponseFilters",
-                    "de.cismet.cids.server.api.tools.CORSResponseFilter");
-                sh.setInitParameter("swagger.version", "1.0");
-                sh.setInitParameter("swagger.api.basepath", swaggerBasePath); // no trailing slash please
-                server = new Server(port);
+            sh.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature",
+                "true");
+            sh.setInitParameter(
+                "com.sun.jersey.spi.container.ContainerResponseFilters",
+                "de.cismet.cids.server.api.tools.CORSResponseFilter");
+            sh.setInitParameter("swagger.version", "1.0");
+            sh.setInitParameter("swagger.api.basepath", swaggerBasePath); // no trailing slash please
+            server = new Server(port);
 
-                final Context context = new Context(server, "/", Context.SESSIONS);
-                context.addServlet(sh, "/*");
-                final String resoursceBaseDir = this.getClass()
-                            .getClassLoader()
-                            .getResource("de/cismet/cids/server/swagger")
-                            .toExternalForm();
+            final Context context = new Context(server, "/", Context.SESSIONS);
+            context.addServlet(sh, "/*");
+            final String resoursceBaseDir = this.getClass()
+                        .getClassLoader()
+                        .getResource("de/cismet/cids/server/swagger")
+                        .toExternalForm();
 
-                final Context swagger = new Context(server, "/swagger", Context.SESSIONS); // NOI18N
+            final Context swagger = new Context(server, "/swagger", Context.SESSIONS); // NOI18N
 
-                swagger.setHandler(new ResourceHandler());
-                swagger.setResourceBase(resoursceBaseDir);
+            swagger.setHandler(new ResourceHandler());
+            swagger.setResourceBase(resoursceBaseDir);
 
-                final ContextHandlerCollection contexts = new ContextHandlerCollection();
-                contexts.setHandlers(
-                    new Handler[] {
-                        swagger,
-                        context
-                    });
+            final ContextHandlerCollection contexts = new ContextHandlerCollection();
+            contexts.setHandlers(
+                new Handler[] {
+                    swagger,
+                    context
+                });
 
-                server.setHandlers(contexts.getHandlers());
+            server.setHandlers(contexts.getHandlers());
+            server.setConnectors(new Connector[] { getConnector() });
 
-                server.start();
+            server.start();
 
-                if (!interactive) {
-                    System.out.println("Server running non-interactive, use 'kill' to shutdown.");
-                } else {
-                    try {
-                        System.out.println("\n\nServer started. Hit enter to shutdown.");
-                        System.in.read();
-                        server.setStopAtShutdown(true);
-                        System.exit(0);
-                    } catch (final IOException e) {
-                        System.out.println("Server running in background, use 'kill' to shutdown.");
-                    }
+            if (!interactive) {
+                System.out.println("Server running non-interactive, use 'kill' to shutdown.");
+            } else {
+                try {
+                    System.out.println("\n\nServer started. Hit enter to shutdown.");
+                    System.in.read();
+                    server.setStopAtShutdown(true);
+                    System.exit(0);
+                } catch (final IOException e) {
+                    System.out.println("Server running in background, use 'kill' to shutdown.");
                 }
-            } catch (Throwable e) {
-                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (final Throwable t) {
+            t.printStackTrace();
+            if (server != null) {
+                server.setStopAtShutdown(true);
+            }
+            System.exit(1);
         }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  IllegalStateException  DOCUMENT ME!
+     */
+    private Connector getConnector() {
+        final Connector connector;
+        if (sslDebug) {
+            log.warn("server interface is in debug mode, no security applied!"); // NOI18N
+            connector = new SocketConnector();
+        } else {
+            if (log.isInfoEnabled()) {
+                log.info("server interface uses SSL connector");                 // NOI18N
+            }
+
+            try {
+                final SslSocketConnector ssl = new SslSocketConnector();
+                ssl.setMaxIdleTime(30000);
+                ssl.setKeystore(sslKeystoreServer);
+                ssl.setPassword(sslKeystoreServerPassword);
+                ssl.setKeyPassword(sslKeystoreServerKeyPassword);
+
+                if (sslClientAuth) {
+                    ssl.setTruststore(sslKeystoreClient);
+                    ssl.setTrustPassword(sslKeystoreClientPassword);
+                }
+                ssl.setWantClientAuth(sslClientAuth);
+                ssl.setNeedClientAuth(sslClientAuth);
+
+                connector = ssl;
+            } catch (final Exception e) {
+                final String message = "cannot initialise SSL connector"; // NOI18N
+                log.error(message, e);
+                throw new IllegalStateException(message, e);
+            }
+        }
+
+        connector.setPort(port);
+        connector.setHeaderBufferSize(HEADER_BUFFER_SIZE);
+
+        return connector;
     }
 
     /**
